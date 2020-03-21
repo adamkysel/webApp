3ebb1e0dfea152a24feba9e28c2748c850305c32
adamkysel <50201634+adamkysel@users.noreply.github.com>
2020-03-21 19:54:34 +0100

    first

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var Cloudant = require('@cloudant/cloudant');
-var cloudant = Cloudant({ username:'a5fece7f-3038-48f1-ac5b-87aec575ffae-bluemix', password:'ee60e0c476522a2f1f0a365be212c45827f41f428a41e21b6b2f44deaf09c2e6', url:'https://a5fece7f-3038-48f1-ac5b-87aec575ffae-bluemix:ee60e0c476522a2f1f0a365be212c45827f41f428a41e21b6b2f44deaf09c2e6@a5fece7f-3038-48f1-ac5b-87aec575ffae-bluemix.cloudantnosqldb.appdomain.cloud' });
-var db = cloudant.db.use('diplo1423')
+var cloudant = Cloudant({ username:'a8319e14-276b-4500-9437-6012195a6c59-bluemix', password:'dd55554a1c9962c6c542656de61a2bea205555ac3cac1ca43c1d0991bb377e84', url:'https://a8319e14-276b-4500-9437-6012195a6c59-bluemix:dd55554a1c9962c6c542656de61a2bea205555ac3cac1ca43c1d0991bb377e84@a8319e14-276b-4500-9437-6012195a6c59-bluemix.cloudantnosqldb.appdomain.cloud' });
+var db = cloudant.db.use('diplo')
+var db_control = cloudant.db.use('diplo1423')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => {
+    res.render('pages/index')})
   .get('/goro', (req, res) => {
     var hums = [];
     var temps = [];
-    /*db.insert({ _id: 'motor1',_rev: '2-25c59eb0ad7f88b92c840f582ae3709a', state: 'open'}, function (err, body) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(body)
-      }
-    })*/
+    var soils1 = [];
+    var soils2 = [];
+    var soils3 = [];
+    var soils4 = [];
+    var datimes = [];
+
     db.list(async function (err, body) {
       if (err) {
         console.log(err)
       } else {
-        //console.log(body)
-        for(i=0;i<30;i++){
+        for(i=0;i<10;i++){
           await db.get(body.rows[i].id, async function(err, body) {
-        
-            if (!err && body.te !== null && body.te !== undefined)
+            if (!err && body.temperature !== null && body.temperature !== undefined)
             {
-              temps.push(body.te);
+              temps.push(body.temperature);
               console.log(temps);
             }
-              //console.log(typeof temps);
-              //console.log(body.te);
+
+            if (!err && body.humidity !== null && body.humidity !== undefined)
+            {
+              hums.push(body.humidity);
+              console.log(hums);
+            }
+
+            if (!err && body.soil1 !== null && body.soil1 !== undefined)
+            {
+              soils1.push(body.soil1);
+              console.log(soils1);
+            }
+
+            if (!err && body.soil2 !== null && body.soil2 !== undefined)
+            {
+              soils2.push(body.soil2);
+              console.log(soils2);
+            }
+            if (!err && body.soil3 !== null && body.soil3 !== undefined)
+            {
+              soils3.push(body.soil3);
+              console.log(soils3);
+            }
+            if (!err && body.soil4 !== null && body.soil4 !== undefined)
+            {
+              soils1.push(body.soil4);
+              console.log(soils4);
+            }
+            if (!err && body.datetime !== null && body.datetime !== undefined)
+            {
+              datimes.push(body.datetime);
+              console.log(datimes);
+            }
           })
         }     
       }
     })
   
-    //console.log(temps);
-    setTimeout(function(){ res.render('pages/graph',{temp:temps}); }, 1800);
+    setTimeout(function(){ res.render('pages/graph',
+    { temps:temps, 
+      hums:hums,
+      datimes:datimes,
+      soils1:soils1, 
+      soils2:soils2, 
+      soils3:soils3, 
+      soils4:soils4}); }, 1800);
   })
 
-  .post('/open1', (req, res) => {
-    db.get('motor1',async function(err, body){
-      if(body.state ==='open'){
-        await db.insert({ _id: 'motor1',_rev: body._rev, state: 'close'}, function (err, body) {
+  .post('/control1', (req, res) => {
+    db_control.get('commands',async function(err, body){
+      if(body.motor1 ==='open'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1: 'close', motor2:body.motor2, relay:body.relay}, function (err, body) {
           if (err) {
             console.log(err)
           } else {
@@ -57,8 +91,8 @@ express()
           }
         })
       }
-      if(body.state ==='close'){
-        await db.insert({ _id: 'motor1',_rev: body._rev, state: 'open'}, function (err, body) {
+      if(body.motor1 ==='close'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1: 'open', motor2:body.motor2, relay:body.relay}, function (err, body) {
           if (err) {
             console.log(err)
           } else {
@@ -69,24 +103,70 @@ express()
       res.sendStatus(200)
     })
   })
-  .get('/state1', (req, res) => {
-    db.get('motor1',async function(err, body){
-      res.send(body);
-    })
-    })
-
-    /*.post('/close1', (req, res) => {
-      db.get('motor1',async function(err, body){
-        await db.insert({ _id: 'motor1',_rev: body._rev, state: 'close'}, function (err, body) {
+  .post('/control2', (req, res) => {
+    db_control.get('commands',async function(err, body){
+      if(body.motor2 ==='open'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1:body.motor1, motor2: 'close', relay:body.relay}, function (err, body) {
           if (err) {
             console.log(err)
           } else {
             console.log(body)
           }
         })
-        res.sendStatus(200)
-      })
-    })*/
+      }
+      if(body.motor2 ==='close'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1:body.motor1, motor2: 'open', relay:body.relay}, function (err, body) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(body)
+          }
+        })
+      }
+      res.sendStatus(200)
+    })
+  })
+  .post('/control3', (req, res) => {
+    db_control.get('commands',async function(err, body){
+      if(body.relay ==='off'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1:body.motor1, motor2:body.motor2, relay: 'on'}, function (err, body) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(body)
+          }
+        })
+      }
+      if(body.relay ==='on'){
+        await db_control.insert({ _id: 'commands',_rev: body._rev, motor1:body.motor1, motor2:body.motor2, relay: 'off'}, function (err, body) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(body)
+          }
+        })
+      }
+      res.sendStatus(200)
+    })
+  })
+
+  .get('/state1', (req, res) => {
+    var hums = [];
+    var temps = [];
+    var motor1 = "";
+    var motor2 = "";
+    var relay = "";
+
+
+    db_control.get('commands',async function(err, body){
+      motor1 = body.motor1;
+      motor2 = body.motor2;
+      relay = body.relay;
+    })
+
+    console.log(motor1)
+    console.log(motor2)
+    setTimeout(function(){ res.send({motor1:motor1,motor2:motor2}); }, 2000);})
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
